4d317a0e4d64f9675c3c3058176e9377d7a29576
CoryJohnson101 <cory.johnson4545@gmail.com>
2023-07-15 01:04:32 -0400

    uhoh

--- GoogSetup.py
+++ GoogSetup.py
@@ -1,30 +1,78 @@
-from urllib import request
 import requests
+from Google import Create_Service
+import os
 
-with open("resources/PoolNum.txt", "w+") as f:
-    lines = f.readlines()
-    f.write(lines[0] + 1)
 def CreateGoogleSheet():
 
-    response = requests.get("https://sheets.googleapis.com/v4/spreadsheets/1zi9UaJCWSYMNuePZS3JNNK6-XM7vz_pNgc0Dt0ikOfw")
-    sheet = response.text
+    CLIENT_SECRET_FILE = 'credentials.json'
+    API_NAME = 'sheets'
+    API_VERSION = 'v4'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    with open("resources/PoolNum.txt", "r") as f:
+        lines = f.readlines()
+    with open("resources/PoolNum.txt", "w") as f:
+        num = int(lines[0])
+        num = num + 1
+        x = str(num)
+        f.write(x)
 
     SPREADSHEET_ID = "1zi9UaJCWSYMNuePZS3JNNK6-XM7vz_pNgc0Dt0ikOfw"
     OLD_SHEET_ID = 1159631575
     NEW_SHEET_NAME = "Pool #" + lines[0]
 
-    # Insert New Empty Row at A2
-    body = {
-        'requests': [
+    request_body = {
+        'requests' : [
             {
-                'duplicateSheet': {
-                    'sourceSheetId': OLD_SHEET_ID,
-                    'insertSheetIndex': 2,
-                    'newSheetName': NEW_SHEET_NAME
+                'duplicateSheet' : {
+                    'sourceSheetId' : OLD_SHEET_ID,
+                    'newSheetName' : NEW_SHEET_NAME,
+                    'insertSheetIndex' : 2
                 }
             }
         ]
     }
-    # Using sheet.values().batchUpdate will trigger: Invalid JSON payload received. Unknown name "requests": Cannot find field.
-    result = sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID,
-                               body=body).execute()
+
+    response = service.spreadsheets().batchUpdate(
+        spreadsheetId=SPREADSHEET_ID,
+        body=request_body
+    ).execute()
+
+def populateSheet():
+
+    with open("resources/PoolNum.txt", "r") as f:
+        lines = f.readlines()
+
+    num = int(lines[0])
+    num = num - 1
+    NEW_SHEET_NAME = "Pool #" + num
+    CLIENT_SECRET_FILE = 'credentials.json'
+    API_NAME = 'sheets'
+    API_VERSION = 'v4'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    SPREADSHEET_ID = "1zi9UaJCWSYMNuePZS3JNNK6-XM7vz_pNgc0Dt0ikOfw"
+    worksheet_name = NEW_SHEET_NAME
+
+    cell_range_insert = 'A2'
+    values = getInfo()
+    value_range_body = {
+        'majorDimension': 'ROWS',
+        'values' : values
+    }
+
+    service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID,
+        valueInputOption='USER ENTERED',
+        range=worksheet_name + cell_range_insert,
+        body=value_range_body
+    ).execute
+
+
+def getInfo():
+    values = []
+    return values
--- updateButton.py
+++ updateButton.py
@@ -19,13 +19,46 @@ def UpdateImageList():
     for x in lines:
         temp = lines[index].strip()
         response = requests.get("https://pokeapi.co/api/v2/pokemon/" + temp)
-        print(response.status_code)
         data = response.text
         parse_json = json.loads(data)
         urllib.request.urlretrieve(parse_json["sprites"]["other"]["official-artwork"]["front_default"],
                                    "resources/pics/" + temp + ".png")
+        print(index)
         index = index + 1
     print("done")
+
+
+#this is probably trash
+    for x in images:
+        f = open("resources/images.txt", "a")
+        f.write(images[index2] + "\n")
+        f.close()
+        index2 = index2 + 1
+        print("added" + str(index2))
+
+# UpdateImageList()
+def UpdateSpriteList():
+    images = []
+    index = 0
+    index2 = 0
+    shutil.rmtree('resources/sprites')
+    os.mkdir('resources/sprites')
+    for x in lines:
+        try:
+            temp = lines[index].strip()
+            response = requests.get("https://pokeapi.co/api/v2/pokemon/" + temp)
+            data = response.text
+            parse_json = json.loads(data)
+            urllib.request.urlretrieve(parse_json["sprites"]["front_default"], "resources/sprites/" + temp + ".png")
+            print(index)
+            index = index + 1
+        except TypeError as e:
+            temp = "dedenne"
+            response = requests.get("https://pokeapi.co/api/v2/pokemon/" + temp)
+            data = response.text
+            parse_json = json.loads(data)
+            urllib.request.urlretrieve(parse_json["sprites"]["front_default"], "resources/sprites/" + temp + ".png")
+    print("done part 2")
 
     for x in images:
         f = open("resources/images.txt", "a")
@@ -34,4 +67,5 @@ def UpdateImageList():
         index2 = index2 + 1
         print("added" + str(index2))
 
+UpdateSpriteList()
 # def UpdatePokemonList():
